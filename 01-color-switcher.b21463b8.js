cdf0b2a759840386d13231b10eddb1186b94f0df
DorotaOr <141347020+DorotaOr@users.noreply.github.com>
2024-02-12 17:38:06 +0000

    Deploying to gh-pages from @ DorotaOr/goit-js-hw-09@583c758f90c80a5e42f1c5f9bffdcc5f762ab460 🚀

---
+++ 01-color-switcher.b21463b8.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{e.disabled=!0,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,clearInterval(d)}))}));
+//# sourceMappingURL=01-color-switcher.b21463b8.js.map
